aa2b2ea8ef95d71312aeb61a51217a3ebcd15462
amy1181996 <amy1181996@gmail.com>
2023-09-15 11:31:51 +0300

    changed current week service

--- lib/data/service/current_week_service.dart
+++ lib/data/service/current_week_service.dart
@@ -1,27 +1,18 @@
-import 'package:bsuir_schedule/data/api/current_week_api.dart';
-import 'package:bsuir_schedule/data/shared_preferences/current_week_shared_prefs.dart';
-
 class CurrentWeekService {
-  static final CurrentWeekApi _currentWeekApi = CurrentWeekApi();
-  static final CurrentWeekSharedPrefs _currentWeekSharedPrefs =
-      CurrentWeekSharedPrefs();
-
   Future<int?> getCurrentWeek() async {
-    final currentWeekSharedPrefs =
-        await _currentWeekSharedPrefs.getCurrentWeek();
-    int? currentWeek = currentWeekSharedPrefs.key;
-    final currentWeekSetDate = currentWeekSharedPrefs.value;
+    final now = DateTime.now();
+    final initialDate = DateTime(now.year, 9, 1);
 
-    if (currentWeek == null) {
-      currentWeek = await _currentWeekApi.getCurrentWeek() ?? 1;
-    } else {
-      final weeksDelta =
-          DateTime.now().difference(currentWeekSetDate).inDays ~/ 7;
-      currentWeek = (currentWeek + weeksDelta - 1) % 4 + 1;
+    int difference = now.difference(initialDate).inDays;
+
+    final int daysUntilMonday = (8 - initialDate.weekday) % 7;
+
+    if (difference < daysUntilMonday) {
+      return 1;
     }
 
-    await _currentWeekSharedPrefs.setCurrentWeek(currentWeek);
-
+    difference -= daysUntilMonday;
+    final currentWeek = (difference ~/ 7 + 1) % 4 + 1;
     return currentWeek;
   }
 }
